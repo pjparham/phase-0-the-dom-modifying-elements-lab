61601a60305d42fa2eb49c1c4371c67f926a176b
Parker Parham <parkerparham@gmail.com>
2022-07-25 17:42:04 -0400

    commit tree 28f0270b67bc509e12407e0f9ae39b8155912838

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Write your code here!
-const main = document.querySelector('main#main') //this selects the specified line
-//console.log(main)
-main.parentElement.removeChild(main); //this tells it to delete that line
-const newHeader = document.createElement('h1') //don't forget the '' && remember importance of document.createElement()
-newHeader.id = "victory" //this syntax of element.append
-newHeader.innerHTML = "Parker is the champion" //innerHTML allows us to set the HTML value of a property
-// newHeader.textContent = "Parker is the champion" //This line also passes the test!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Parker is the champion"
